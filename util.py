4b26beba15282f0a79130a5e3f86f735d8cf1caf
Emilio Rojas <emilio93@gmail.com>
2019-06-25 02:00:49 -0600

    Update variable names

--- util.py
+++ util.py
@@ -26,20 +26,20 @@ def parseParameters(argv, DEBUG):
     FN_DEBUG = False
 
     # Parametros por defecto para el servidor y cliente.
-    TCP_IP = '127.0.0.1' # Direccion Ip
-    TCP_PORT = 5005      # Puerto
-    BUFFER_SIZE = 32     # Tamano del buffer
-    HELP_FLAG = True     # Bandera de ayuda(para imprimir texto de ayuda).
+    connectionIp = '127.0.0.1'  # Direccion Ip
+    connectionPort = 5005      # Puerto
+    bufferSize = 32     # Tamano del buffer
+    helpFlag = True     # Bandera de ayuda(para imprimir texto de ayuda).
 
     # Imprimir informacion de argumentos y valores por defecto.
     if (DEBUG and FN_DEBUG):
         print ("argv:")
         print argv
         print ("\nValores por Defecto:")
-        print ("  Ip: %s" % (TCP_IP))
-        print ("  Puerto: %s" % (TCP_PORT))
-        print ("  Tamano de buffer: %s" % (BUFFER_SIZE))
-        print ("  Help Flag: %s\n" % (HELP_FLAG))
+        print ("  Ip: %s" % (connectionIp))
+        print ("  Puerto: %s" % (connectionPort))
+        print ("  Tamano de buffer: %s" % (bufferSize))
+        print ("  Help Flag: %s\n" % (helpFlag))
 
     # Contador para while.
     i = 0
@@ -54,42 +54,42 @@ def parseParameters(argv, DEBUG):
         # luego de la bandera, si este argumento no existe, la direccion IP
         # no cambia.
         if ((i < argvLen-1) and (argv[i] == '-h' or argv[i] == '--host-ip')):
-            TCP_IP = argv[i+1]
-            HELP_FLAG = False
+            connectionIp = argv[i+1]
+            helpFlag = False
 
         # Del mismo modo, las bandera -p o --port, cambian el puerto.
         elif ((i < argvLen-1) and (argv[i] == '-p' or argv[i] == '--port')):
-            TCP_PORT = argv[i+1]
-            HELP_FLAG = False
+            connectionPort = argv[i+1]
+            helpFlag = False
 
         # Del mismo modo, las bandera -s o --buffer-size, cambian el tamano del buffer.
         elif ((i < argvLen-1) and (argv[i] == '-s' or argv[i] == '--buffer-size')):
-            BUFFER_SIZE = argv[i+1]
-            HELP_FLAG = False
+            bufferSize = argv[i+1]
+            helpFlag = False
 
         # Por defecto el valor de la bandera de ayuda es True,
         # si no ha sido bajada, se mantiene en alto.
         # Para imprimir la ayuda, solo esta bandera debe estar.
         elif (argv[i] == '--help'):
-            HELP_FLAG = HELP_FLAG
+            helpFlag = helpFlag
 
         # Se incrementa el contador de argumentos.
         i = i+1
 
     # Bajar la bandera de ayuda si no se han pasado argumentos.
     if (argvLen <= 1):
-        HELP_FLAG = False
+        helpFlag = False
 
     # Imprimir la informacion de los datos a utilizar posterior a procesar los
     # argumentos.
     if (DEBUG and FN_DEBUG):
         print ("Valores a Utilizar:\n  Ip: %s\n  Puerto: %s" %
-               (TCP_IP, TCP_PORT))
+               (connectionIp, connectionPort))
         print ("  Tamano de buffer: %s\n  Help Flag: %s\n" %
-               (BUFFER_SIZE, HELP_FLAG))
+               (bufferSize, helpFlag))
 
     # Se devuelve los tados de direccion ip, puerto, tamano de buffer y bandera de ayuda.
-    return [TCP_IP, TCP_PORT, BUFFER_SIZE, HELP_FLAG]
+    return connectionIp, connectionPort, bufferSize, helpFlag
 
 
 def getMessage():
