1f3d185490e4f2eb7be127fe265d906329bd899d
Emilio Rojas <emilio93@gmail.com>
2019-07-06 19:47:46 -0600

    Update simple udpServer

--- src/simpleUdpServer.py
+++ src/simpleUdpServer.py
@@ -3,14 +3,13 @@
 import socket
 import sys
 
-connectionIp='127.0.0.1'
-connectionPort=5005
-bufferSize=32
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((connectionIp, connectionPort))
+connectionIp = '127.0.0.1'
+connectionPort = 5005
+bufferSize = 32
 
 while (1):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((connectionIp, connectionPort))
     receivedData, addr = sock.recvfrom(int(bufferSize))
     response = receivedData.upper()
     sock.sendto(response, addr)
